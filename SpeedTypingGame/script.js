c954f0c53cc9127ad103fbcd973d83976b810c76
lllieutrieuthuan <lieutrieuthuan14@gmail.com>
2020-11-01 21:42:14 +0700

    typing game

---
+++ SpeedTypingGame/script.js
@@ -0,0 +1,66 @@
+const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random"
+const quoteDisplayElement = document.getElementById('quote-display')
+const quoteInputElement = document.getElementById('quote-input')
+const timerElement = document.getElementById('timer')
+
+
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTE_API_URL).then(res => res.json()).then(data => data.content)
+}
+
+
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+  const arrayValue = quoteInputElement.value.split('')
+
+  let correct = true
+
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character === null) {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if (characterSpan.innerText === character) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false
+    }
+  })
+  if (correct) { getNextQuote() }
+})
+
+
+
+async function getNextQuote() {
+  const quote = await getRandomQuote()
+  quoteDisplayElement.innerHTML = ''
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span')
+    characterSpan.classList.add('correct')
+    characterSpan.innerText = character
+    quoteDisplayElement.appendChild(characterSpan)
+  })
+  quoteInputElement.value = null
+  startTimer()
+
+}
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timerElement.innerText = getTimerTime()
+  }, 1000)
+}
+
+
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
+}
+
+getNextQuote()
